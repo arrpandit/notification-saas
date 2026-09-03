b707f7cddfacda22cff601b7ab8590dd2ab8913f
Alok Ranjan Pandit <alokranjanpandit005@gmail.com>
2026-09-03 21:47:09 +0530

    feat: refactor port assignment in worker service bootstrap function

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -4,7 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(WorkerModule);
-  await app.listen(process.env.port ?? 3001);
+
+  const port = process.env.port || 3001;
+  await app.listen(port, '0.0.0.0');
   Logger.log(`Worker service is running on: ${await app.getUrl()}`);
 }
 
